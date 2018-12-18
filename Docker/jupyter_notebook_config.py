8ac2377e4dd7fa8bda5335dcbf62c0a0a2e5d0e6
msalvaris <msalvaris@users.noreply.github.com>
2018-12-18 10:01:21 +0000

    Removing jupytext

--- Docker/jupyter_notebook_config.py
+++ Docker/jupyter_notebook_config.py
@@ -1,6 +1,5 @@
 # Configuration file for jupyter-notebook.
-c.NotebookApp.contents_manager_class = "jupytext.TextFileContentsManager"
-c.ContentsManager.default_jupytext_formats = "ipynb,py"
+
 c.NotebookApp.ip = "0.0.0.0"
 c.NotebookApp.port = 9999
 c.NotebookApp.open_browser = False
